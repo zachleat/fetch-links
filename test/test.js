8e5fce8e8e964855264091ecbe400249054630f1
Zach Leatherman <zachleat@users.noreply.github.com>
2026-03-31 16:19:47 -0500

    Add test for searchParams

--- test/test.js
+++ test/test.js
@@ -18,3 +18,10 @@ test("zachleat.com", async (t) => {
 	// console.log( filtered );
 	assert.ok(filtered.length > 0);
 });
+
+test("searchParams", async (t) => {
+	let originalUrl = "https://www.11ty.dev/?q=1";
+
+	let filtered = await Links.find(originalUrl);
+	assert.strictEqual(filtered.find(entry => entry.type !== "feed" && entry.url.startsWith("https://www.11ty.dev/")), undefined);
+});
